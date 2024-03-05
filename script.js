96b1c48f3ed8b32f7bcc46a9ca5c949d1d4e6e37
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2024-03-05 11:16:22 +0530

    Add File

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const wrapper = document.querySelector(".wrapper");
+const refreshBtn = document.querySelector(".refresh_btn");
+
+const maxBoxes = 20;
+
+const generatePalette = () => {
+    wrapper.innerHTML = ""; //clearing the wrapper
+    for (let i = 0; i < maxBoxes; i++) {
+        //generating a random hex code for color
+        let randomHexValue = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHexValue = `#${randomHexValue.padStart(6, "0")}`;
+        let randomHexValue2 = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHexValue2 = `#${randomHexValue2.padStart(6, "0")}`;
+
+        //creating a new "li" element add it to the wrapper
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `<div class="box" style="background: linear-gradient(-45deg, ${randomHexValue}, ${randomHexValue2});"></div>
+        <span class="hex-value">${randomHexValue},${randomHexValue2}</span>`;
+        color.addEventListener("click", () => copyColor(color, `${randomHexValue}, ${randomHexValue2}`));
+        wrapper.appendChild(color);
+    }
+}
+
+generatePalette();
+
+const copyColor = (elem, HexValue) => {
+    const colorElement = elem.querySelector(".hex-value")
+    navigator.clipboard.writeText(HexValue).then(() => {
+        colorElement.innerText = "Copied"
+        setTimeout(() => colorElement.innerText = HexValue, 1000)
+    }).catch(() => alert("Failed to copy the color code!"))
+}
+
+refreshBtn.addEventListener("click", generatePalette);
